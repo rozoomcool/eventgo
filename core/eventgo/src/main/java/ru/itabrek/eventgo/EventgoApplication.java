65c139f661cb4d997fc4c2314902534d65f038af
rozoomcool <rozoom.um@gmail.com>
2023-01-18 21:17:53 +0300

    start...

---
+++ core/eventgo/src/main/java/ru/itabrek/eventgo/EventgoApplication.java
@@ -0,0 +1,13 @@
+package ru.itabrek.eventgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventgoApplication.class, args);
+	}
+
+}
---
+++ core/eventgo/src/test/java/ru/itabrek/eventgo/EventgoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itabrek.eventgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
